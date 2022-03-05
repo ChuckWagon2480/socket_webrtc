4af970b7343afb1653081e1e16c31ad325bee19c
ChuckWagon2480 <ghwar100@gmail.com>
2022-03-05 12:23:25 +0900

    Add feature: mute/unmute, camera on/off

--- src/public/js/video.js
+++ src/public/js/video.js
@@ -1,6 +1,10 @@
 const myVideo = document.getElementById('myVideo');
+const soundButton = document.getElementById('sound');
+const cameraButton = document.getElementById('camera');
 
 let myStream;
+let isMute = false;
+let isCameraOff = false;
 
 async function getMedia() {
   try {
@@ -14,4 +18,31 @@ async function getMedia() {
   }
 }
 
+function soundHandler() {
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (!isMute) {
+    soundButton.innerText = 'Unmute';
+    isMute = true;
+  } else {
+    soundButton.innerText = 'Mute';
+    isMute = false;
+  }
+}
+function cameraHandler() {
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (!isCameraOff) {
+    cameraButton.innerText = 'Camera On';
+    isCameraOff = true;
+  } else {
+    cameraButton.innerText = 'Camera Off';
+    isCameraOff = false;
+  }
+}
+
 getMedia();
+soundButton.addEventListener('click', soundHandler);
+cameraButton.addEventListener('click', cameraHandler);
